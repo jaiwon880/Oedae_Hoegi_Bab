c05a0b417b70da91762969ebcff7762277dfe05a
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-04-20 14:22:35 +0900

    update

--- data/place.py
+++ data/place.py
@@ -3,4 +3,5 @@ import streamlit as st
 
 @st.cache_data
 def get_place(n=''):
-    return pd.read_csv('data/oedae_place.csv').sample(frac=1), pd.read_csv('data/hoegi_place.csv').sample(frac=1)
+    return pd.read_csv('data/oedae_place.csv').sample(frac=1)
+    # , pd.read_csv('data/hoegi_place.csv').sample(frac=1)
