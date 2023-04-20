1cb69097160681770563a148c30deb873c2d1157
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-04-20 11:41:03 +0900

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
 from widget import location
+
+
 
 def app():
     st.set_page_config(
@@ -17,16 +18,6 @@ def app():
     location.map()
 
 
-with st.sidebar:
-    choice = option_menu("Menu", ["홈", "검색", "후기"],
-                        icons=['house', 'house', 'house'],
-                        menu_icon="app-indicator", default_index=0,
-                        styles={
-    "container": {"padding": "4!important", "background-color": "#FAFAFA"},
-    "icon": {"color": "black", "font-size": "25px"},
-    "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#FAFAFA"},
-    "nav-link-selected": {"background-color": "#08C7B4"},
-})
 
 if __name__ == '__main__':
     app()
