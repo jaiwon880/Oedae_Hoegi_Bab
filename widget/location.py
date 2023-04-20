ed5a59309544152509a6ce241aba5c49d4e3ac3a
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-04-20 12:32:43 +0900

    update

--- widget/location.py
+++ widget/location.py
@@ -18,9 +18,9 @@ def map():
         }, axis=1).iloc[:,0:3]
     m = folium.Map(
         location=center,
-        min_zoom=16,
+        min_zoom=15,
         max_zoom=30,
-        zoom_start=16,
+        zoom_start=15,
         zoom_control=True,
     )
 
